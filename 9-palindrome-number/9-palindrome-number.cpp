174bb4211c1982505456af9eddbe5954e0b84369
Dipanshi Bansal <dipanshibansal@gmail.com>
2022-03-27 11:48:24 +0530

    Time: 33 ms (16.85%), Space: 5.9 MB (33.27%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
+        if((x % 10 == 0 && x != 0) || x < 0) return false;
         int num = x;
         long long int new_num = 0;
-        while(num > 0) {
+        while(num > new_num) {
             new_num = new_num*10 + num%10;
             num = num / 10;
         }
-        return new_num == x;
+        return (new_num == num || num == new_num/10);
     }
 };
