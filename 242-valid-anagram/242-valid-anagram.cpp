e40e49bb1732752dc122c247d81e308786bd6d12
Dipanshi Bansal <dipanshibansal@gmail.com>
2022-03-27 11:16:52 +0530

    Time: 6 ms (89.05%), Space: 7.4 MB (46.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
-    }
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;
+        
+        int arr[26] = {0};
+        for(int i=0; i<s.size(); i++) {
+            arr[s[i]-'a']++;
+        }
+        for(int i=0; i<t.size(); i++) {
+            arr[t[i]-'a']--;
+        }
+        for(int i=0; i<sizeof(arr)/sizeof(int); i++) {
+            if(arr[i] != 0) return false;
+        }
+        return true;
+     }
 };
