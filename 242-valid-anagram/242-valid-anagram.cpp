9298e9c5ea55f33443eb03bcfc9c2ad1fe238847
Dipanshi Bansal <dipanshibansal@gmail.com>
2022-03-27 11:19:41 +0530

    Time: 6 ms (89.05%), Space: 7.4 MB (46.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,14 +4,15 @@ public:
         // sort(s.begin(), s.end());
         // sort(t.begin(), t.end());
         // return s == t;
-        
+        if(s.size() != t.size()) return false;
         int arr[26] = {0};
         for(int i=0; i<s.size(); i++) {
             arr[s[i]-'a']++;
-        }
-        for(int i=0; i<t.size(); i++) {
             arr[t[i]-'a']--;
         }
+        // for(int i=0; i<t.size(); i++) {
+        //     arr[t[i]-'a']--;
+        // }
         for(int i=0; i<sizeof(arr)/sizeof(int); i++) {
             if(arr[i] != 0) return false;
         }
