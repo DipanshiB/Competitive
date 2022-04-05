cb0e2a2fff42ac8f566854d8272d63214ee45ca7
Dipanshi Bansal <dipanshibansal@gmail.com>
2022-04-05 22:19:04 +0530

    Time: 153 ms (23.72%), Space: 59.1 MB (38.72%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    // static bool comp(pair<int, int>& a, pair<int, int>& b) {
+    //     if(a.second > b.second) return true;
+    //     else return false;
+    // }
+    
+    int maxArea(vector<int>& height) {
+        // vector<pair<int, int>> vec;
+        // for(int i=0; i<height.size(); i++) {
+        //     vec.push_back({i, height[i]});
+        // }
+        // sort(vec.begin(), vec.end(), comp);
+        // int area = 0;
+        // for(int i=0; i<vec.size()-1; i++) {
+        //     for(int j=i+1; j<vec.size(); j++) {
+        //         area = max(area, (abs(vec[i].first - vec[j].first) * min(vec[i].second, vec[j].second)));   
+        //     }
+        // }
+        // return area;
+        
+        int start = 0, end = height.size()-1;
+        int area = 0;
+        while(start < end) {
+            if(height[start] < height[end]) {
+                area = max(area, height[start] * abs(end - start));
+                start++;
+            }
+            else if (height[start] > height[end]) {
+                area = max(area, height[end] * abs(end - start));
+                end--;
+            }
+            else {
+                area = max(area, height[end] * abs(end - start));
+                if(height[start+1] > height[end-1]) start++;
+                else end--;
+            }
+        }
+        return area;
+    }
+};
